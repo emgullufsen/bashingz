1a3e4806cfc1860303cd3eea06b31711e90a1bb0
rickyman <ericgullufsen@gmail.com>
2021-06-18 16:13:42 -0800

    nada much yet on this first exercise

---
+++ ex-2-2-2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# write a script that shows time and date, lists all logged-in users, and
+# gives the system uptime. then saves all this to a logfile
+
+dateval=`date`
+
+
+
+echo "$dateval"
