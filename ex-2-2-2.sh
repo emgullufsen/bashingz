6699a609b3b27ae85edfd4b62324b45fd29df31e
rickygeeman <ricky@rickysquid.org>
2021-06-21 13:07:10 -0800

    exercise 2.2 2

--- ex-2-2-2.sh
+++ ex-2-2-2.sh
@@ -2,8 +2,11 @@
 # write a script that shows time and date, lists all logged-in users, and
 # gives the system uptime. then saves all this to a logfile
 
-dateval=`date`
-
-
-
-echo "$dateval"
+filey=/tmp/ex2-2-2-info.txt
+datey="Date is: $(date)"
+logged_intro="Currently logged in users are:" 
+echo $datey | tee $filey
+echo $logged_intro | tee -a $filey
+who | tee -a $filey
+echo "Current Uptime:"
+uptime | tee -a $filey
