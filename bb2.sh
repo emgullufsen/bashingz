a1366606d244c88e310d5dc4ddecce21ed29a3d7
rickygeeman <ricky@rickysquid.org>
2021-07-18 23:32:32 -0800

    using awk!

--- bb2.sh
+++ bb2.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
-filey=data_bb2.json
+#filey=data_bb2.json
 keyey=$(cat key_bb2.key)
 urley=sportspage-feeds.p.rapidapi.com
 curl --request GET \
 	--url https://${urley}/games \
 	--header "x-rapidapi-host: ${urley}" \
-	--header "x-rapidapi-key: ${keyey}" \
-	-o ${filey}
+	--header "x-rapidapi-key: ${keyey}" |
+	#-o ${filey}
 jq -rc '.results[] | 
-	[.teams.away.team, .scoreboard.score.away, .teams.home.team, 
-	.scoreboard.score.home] | @tsv' \
-	$filey
+	[.teams.home.team, .scoreboard.score.home, 
+	 .teams.away.team, .scoreboard.score.away] | @tsv' | \
+	#$filey | \
+awk -F'\t' '{ print "Home Team: ", $1, " - Away Team: ", $3; }'
